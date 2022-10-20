563f2ba464964e2d5517d1726dafc8f0b3e23a0d
Adam-Isaak-dev <67387234+Adam-Isaak-dev@users.noreply.github.com>
2022-10-20 12:54:37 -0500

    Rename page header to document header for clearer language

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import PageHeader from "../components/PageHeader";
+import DocumentHeader from "../components/DocumentHeader";
 import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
     <div>
-      <PageHeader title="Home" description="my personal portfolio website"/>
+      <DocumentHeader title="Home" description="my personal portfolio website"/>
       <main>
         <div className="h-full h-screen bg-slate-900">
           <NavBar />
