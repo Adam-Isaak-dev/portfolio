16c991a123828fea188b55c262a3bd841e2d7045
Adam-Isaak-dev <67387234+Adam-Isaak-dev@users.noreply.github.com>
2022-10-20 12:52:20 -0500

    Add VT323 font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        logo: ['VT323','monospace']
+      },
+    },
   },
   plugins: [],
 }
